aa3b8d89a992efe18d037dab642ac036d313e8e3
g-franklinn <gfranklinn@hotmail.com>
2024-06-10 18:21:55 -0300

    add: MatEstoque class

---
+++ Materiais/MatEstoque.hpp
@@ -0,0 +1,27 @@
+#ifndef MATESTOQUE
+#define MATESTOQUE
+
+#include <string>
+#include "../Material/Material.hpp"
+
+using std::string;
+
+class MatEstoque: public Material{
+    private:
+        int quantidade;
+        int idLocal;
+        string nomeLocal;
+
+    public:
+        MatEstoque(int quantidade, int id, string nome);   //Construtor padrão
+
+        int getQuantidade();
+        getLocal();
+        baixaEstoque();
+        adicionar();
+        setLocal();
+
+        virtual ~MatEstoque();         // Destrutor
+};
+
+#endif
