99a8c73b78b37564ee85e9e12076286ac96c7629
Barbara Ribeiro <barbaraluisa678@gmail.com>
2024-06-10 19:26:19 -0300

    Superclasse Funcionarios

---
+++ Funcionarios/Funcionarios.hpp
@@ -0,0 +1,34 @@
+#ifndef FUNCIONARIOS
+#define FUNCIONARIOS 
+
+#include <string> 
+
+using std::string; 
+
+class Funcionarios {
+    private:
+    string nomeFuncionario;
+    int idFuncionario;
+    string funcaoFuncionario;
+
+    public:
+    //Construtor padrão
+    Funcionarios();
+
+    string getNome_Funcionario();
+    void setNome_Funcionario(const string& nomeFuncionario);
+    int getId_Funcionario();
+    void setId_Funcionario(int idFuncionario);
+    string getFuncao_Funcionario();
+    void setFuncao_Funcionario(const string& funcaoFuncionario);
+
+    //Métodos abstratos 
+    virtual void colocarMaterial() = 0;
+    virtual void retirarMaterial() = 0;
+
+    virtual ~Funcionarios(); //Destrutor 
+
+};
+
+
+#endif
